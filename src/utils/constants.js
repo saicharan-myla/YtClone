f46f4cb78c333ea240a1300c68654d366513af36
SaiCharanReddy_Maila <maila.s@northeastern.edu>
2024-01-22 12:17:52 -0500

    added comps

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+const GOOGLE_API_KEY = "AIzaSyB0ReMPBemCmEK7PCbW3ZHjGEyl-9wUlhU";
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
